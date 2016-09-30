a381388de0b47a126119460b3e04068140e045d9
gruppjo <jona_blubb@web.de>
2016-09-30 18:22:21 +0200

    HeroService with DI in AppComponent using providers and constructor, getHeroes(): Promise<Heroes[]>

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -67,24 +68,22 @@ import { Hero } from './hero';
     </ul>
     <my-hero-detail [hero]="selectedHero"></my-hero-detail>
   `,
+  providers: [HeroService]
 })
 
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private heroService: HeroService) { };
   title: string = 'tour of heroes';
-  heroes: Hero[] = [
-    { id: 11, name: 'Mr. Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' }
-  ];
+  heroes: Hero[];
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .then(heroes => this.heroes = heroes);
+  };
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+  };
+  ngOnInit(): void {
+    this.getHeroes();
   }
 }
