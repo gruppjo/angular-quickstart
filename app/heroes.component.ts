ef706eb1c819928dd0a3bdf5657f2550c82be868
gruppjo <jona_blubb@web.de>
2016-10-04 19:36:53 +0200

    update/create/delete with Promises

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -24,13 +24,30 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .then(heroes => this.heroes = heroes);
   };
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+    .then(hero => {
+      this.heroes.push(hero);
+      this.selectedHero = null;
+    })
+  };
+  delete(hero: Hero): void {
+    this.heroService.delete(hero.id)
+      .then(() => {
+        this.heroes = this.heroes.filter(h => h !== hero)
+        if (this.selectedHero === hero) {
+          this.selectedHero = null;
+        }
+      });
+  };
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   };
   ngOnInit(): void {
     this.getHeroes();
   };
-
   gotoDetail(): void {
     let link = ['/detail', this.selectedHero.id];
     this.router.navigate(link);
